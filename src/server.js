8abbca6e410f9e06942589f2b48fa07803440bbb
Danylo <161326427+Daniel2-1@users.noreply.github.com>
2025-11-19 11:44:53 +0100

    Update server.js for MongoDB task

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
-
+// e
 const PORT = process.env.PORT ?? 3000;
 const app = express();
 app.use(cors());
